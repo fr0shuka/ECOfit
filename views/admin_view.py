67e4d4c722e45b5243587a72d71659cf30a3e89c
MiguelBorges <miguelborges.mb@gmail.com>
2026-08-05 22:51:28 +0100

    admin

--- views/admin_view.py
+++ views/admin_view.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from models.user_model import UserModel
 from controllers.admin_controller import AdminController
 
