3b11706718e14f1b1671c4c57e7546437fb46581
Miguel Borges <78087064+fr0shuka@users.noreply.github.com>
2026-09-01 11:14:14 +0000

    my_trainings_views

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from views.admin_analytics_view import AdminAnalyticsView
 from views.my_trainings_view import MyTrainingsView
 from views.components import renderizar_meteo_sidebar
 from services.news_service import renderizar_galeria_eventos
-from controllers.activity_controller import ActivityController
 
 # Configuração da página
 st.set_page_config(
@@ -17,13 +16,7 @@ st.set_page_config(
     layout="centered"
 )
 
-def inicializar_estado():
-    """Garante que todos os controladores e estado inicial existem no session_state."""
-    if 'controller_atividades' not in st.session_state:
-        st.session_state.controller_atividades = ActivityController()
 
-
-inicializar_estado()
 
 # Fluxo de navegação baseado no estado da sessão
 if 'utilizador_logado' not in st.session_state:
